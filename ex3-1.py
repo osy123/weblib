bba9971c7788359b70a5cc7b36255e1c66214a87
osy123 <dhtpdb123@naver.com>
2018-09-21 14:03:21 +0900

    hio

---
+++ ex3-1.py
@@ -0,0 +1,4 @@
+from urllib.parse import urlparse
+
+result = urlparse('http://www.python.org:80/guido/python.html?name=둘리&id=dooley')
+print(result)
---
+++ ex3-2.py
@@ -0,0 +1,8 @@
+#get방식 요청
+from urllib.parse import urlencode
+from urllib.request import urlopen
+
+query = urlencode({'name':'둘리','a': 10, 'b' : 20})
+f = urlopen('http://www.example.com? + query')
+response = f.read()
+print(response)
