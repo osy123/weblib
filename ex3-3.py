58adf200aa4cf127292ed101b2aef6f9f61b08db
osy123 <dhtpdb123@naver.com>
2018-09-21 14:18:48 +0900

    수정

--- ex3-3.py
+++ ex3-3.py
@@ -2,12 +2,20 @@
 from urllib.parse import urlencode
 from urllib.request import Request, urlopen
 
-data = urlencode({'name':'길동이','email':'','pwd':''}) #join페이지에서 이부분에 빈공간으로 하고 for문을 돌리면 자동회원가입이 된다.
-data = data.encode('UTF-8')
+from urllib.parse import urlencode
+from urllib.request import Request, urlopen
 
-request = Request('http://www.example.com/join', data)
-request.add_header('Content-Type', 'text/html')
+data = urlencode({'name': '둘리', 'a': 10, 'b': 20})
+print(data, '\n')
 
-f = urlopen(request)
-response = f.read()
-print(response)
+data = data.encode("UTF-8")  # 바이트 형태로 보내야 하므로 인코딩 필요
+print(data)
+
+req = Request('http://www.example.com', data)
+req.add_header('Contest-Type', 'text/html')
+
+
+f = urlopen(req)
+req = f.read()
+print(req)
+
