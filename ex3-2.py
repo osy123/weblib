f8c6f1268df9c7e29387c3c4f3bf91f16fccc62a
osy123 <dhtpdb123@naver.com>
2018-09-21 15:13:51 +0900

    수정

--- ex3-2.py
+++ ex3-2.py
@@ -2,7 +2,8 @@
 from urllib.parse import urlencode
 from urllib.request import urlopen
 
-query = urlencode({'name':'둘리','a': 10, 'b' : 20})
-f = urlopen('http://www.example.com? + query')
+query = urlencode({'name':'둘리','a':10,'b':20})
+
+f = urlopen('http://www.example.com?' + query)
 response = f.read()
 print(response)
--- myhttpserver2.py
+++ myhttpserver2.py
@@ -3,8 +3,13 @@ from http.server import HTTPServer, BaseHTTPRequestHandler
 port = 7777
 
 class MyHttpRequestHandler(BaseHTTPRequestHandler):
-    pass
-
+    def do_GET(self):
+        req_url = self.path[:self.path.find('?')]
+        print(req_url)
+        self.send_response(200)
+        self.send_header('Content-Type','text/html; charset=utf-8')
+        self.end_headers()
+        self.wfile.write('<h1>TEST</h1>'.encode('utf-8'))
 
 httpd = HTTPServer(('', port), MyHttpRequestHandler)
 print('Server running on port', port)
