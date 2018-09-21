e0f7f6bf1151fe2893c76cfedaa7c23637416c76
osy123 <dhtpdb123@naver.com>
2018-09-21 14:06:50 +0900

    hio

--- ex3-3.py
+++ ex3-3.py
@@ -2,7 +2,7 @@
 from urllib.parse import urlencode
 from urllib.request import Request, urlopen
 
-data = urlencode({'name':'둘리','email':'','pwd':''}) #join페이지에서 이부분에 빈공간으로 하고 for문을 돌리면 자동회원가입이 된다.
+data = urlencode({'name':'길동이','email':'','pwd':''}) #join페이지에서 이부분에 빈공간으로 하고 for문을 돌리면 자동회원가입이 된다.
 data = data.encode('UTF-8')
 
 request = Request('http://www.example.com/join', data)
